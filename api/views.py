ca047c9a28f3cbabcb7d2475f3e7e62d7297bbb3
fawin <facu.1999.10@gmail.com>
2025-02-20 18:12:55 -0300

    last changes

--- api/views.py
+++ api/views.py
@@ -1,9 +1,8 @@
-from django.http import Http404
-from rest_framework import status
-from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from django.contrib.auth.models import User
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from django.http import Http404
 from .models import Product, ProductType
 from .serializer import ProductSerializer, ProductTypeSerializer, UserSerializer
 
