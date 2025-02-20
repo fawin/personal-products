937923e05e5a997dd0813be951e82eded2cc35da
fawin <facu.1999.10@gmail.com>
2025-02-20 18:06:47 -0300

    product detail

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ProductList, ProductTypeList
+from .views import ProductList, ProductTypeList, ProductDetail
 
 
 urlpatterns = [
     path('products/', ProductList.as_view(), name='products'),
+    path('products/<int:pk>/', ProductDetail.as_view(), name='product-detail'),
     path('products-type/', ProductTypeList.as_view(), name='products-type'),
 ]
--- api/views.py
+++ api/views.py
@@ -9,9 +9,10 @@ from .serializer import ProductSerializer, ProductTypeSerializer, UserSerializer
 
 
 # Create your views here.
-
 class UserRegister(APIView):
+    
     permission_classes = [AllowAny]
+    
     def post(self, request, format=None):
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
@@ -34,6 +35,34 @@ class ProductList(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class ProductDetail(APIView):
+    
+    permission_classes = [IsAuthenticated]
+
+    def get_obj(self, pk):
+        try:
+            return Product.objects.get(pk=pk)
+        except Product.DoesNotExist:
+            raise Http404
+    
+    def get(self, request, pk, format=None):
+        product = self.get_obj(pk)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+    
+    def put(self, request, pk, format=None):
+        product = self.get_obj(pk)
+        serializer = ProductSerializer(product, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk, format=None):
+        product = self.get_obj(pk=pk)
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 class ProductTypeList(APIView):
    
